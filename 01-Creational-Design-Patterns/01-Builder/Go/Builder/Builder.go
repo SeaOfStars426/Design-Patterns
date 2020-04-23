b4488e606b8fb757c6d85f3abf4c30ed5f1776d0
Joe Chen <joe.chen.sydney@gmail.com>
2020-04-23 20:56:45 +1000

    Go code for Builder pattern

---
+++ 01-Creational-Design-Patterns/01-Builder/Go/Builder/Builder.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+)
+
+const (
+  indentSize = 2
+)
+type HtmlElement struct {
+  name, text string
+  elements []HtmlElement
+}
+
+func (e *HtmlElement) String() string {
+  return e.string(0)
+}
+
+func (e *HtmlElement) string(indent int) string {
+  sb := strings.Builder{}
+  i := strings.Repeat(" ", indentSize * indent)
+  sb.WriteString(fmt.Sprintf("%s<%s>\n",
+    i, e.name))
+  if len(e.text) > 0 {
+    sb.WriteString(strings.Repeat(" ",
+      indentSize * (indent + 1)))
+    sb.WriteString(e.text)
+    sb.WriteString("\n")
+  }
+
+  for _, el := range e.elements {
+    sb.WriteString(el.string(indent+1))
+  }
+  sb.WriteString(fmt.Sprintf("%s</%s>\n", 
+    i, e.name))
+  return sb.String()
+}
+
+type HtmlBuilder struct {
+  rootName string
+  root HtmlElement
+}
+
+func (b *HtmlBuilder) String() string {
+  return b.root.String()
+}
+
+func NewHtmlBuilder(rootName string) *HtmlBuilder {
+	return &HtmlBuilder{ rootName, 
+	HtmlElement{rootName, "", []HtmlElement{}}}
+}
+
+func (b *HtmlBuilder) AddChild(
+  childName string, childText string) {
+  e := HtmlElement{childName, childText, []HtmlElement{}}
+  b.root.elements = append(
+    b.root.elements, e)
+}
+
+func (b *HtmlBuilder) AddChildFluent(
+  childName string, childText string) *HtmlBuilder {
+  e := HtmlElement{childName, childText, []HtmlElement{}}
+  b.root.elements = append(
+    b.root.elements, e)
+  return b
+}
+
+func (b *HtmlBuilder) Clear() {
+  b.root = HtmlElement{b.rootName, "", []HtmlElement{}}
+}
+
+func main() {
+  // if you want to build a simple HTML paragraph
+  // using a strings.Builder
+  hello := "hello"
+  sb := strings.Builder{}
+  sb.WriteString("<p>")
+  sb.WriteString(hello)
+  sb.WriteString("</p>")
+  fmt.Printf("%s\n", sb.String())
+
+  b := NewHtmlBuilder("ul")
+  b.AddChildFluent("li", "hello").
+	AddChildFluent("li", "world")
+  fmt.Println(b.String())
+}
