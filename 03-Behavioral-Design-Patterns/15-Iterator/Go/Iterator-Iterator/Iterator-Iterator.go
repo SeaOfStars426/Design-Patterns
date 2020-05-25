b0294a631c4e96a2a99e714b219c0b3a92d6481b
Joe Chen <joe.chen.sydney@gmail.com>
2020-05-25 12:30:12 +1000

    Go code for Iterator Pattern, added iterator struct methods

--- 03-Behavioral-Design-Patterns/15-Iterator/Go/Iterator-Iterator/Iterator-Iterator.go
+++ 03-Behavioral-Design-Patterns/15-Iterator/Go/Iterator-Iterator/Iterator-Iterator.go
@@ -23,12 +23,40 @@ func (p *Person) NamesGenerator() <-chan string {
   return out
 }
 
+type PersonNameIterator struct {
+  person *Person
+  current int
+}
+
+func NewPersonNameIterator(person *Person) *PersonNameIterator {
+  return &PersonNameIterator{person, -1}
+}
+
+func (p *PersonNameIterator) MoveNext() bool {
+    p.current++
+    return p.current < 3
+}
+
+func (p *PersonNameIterator) Value() string {
+  switch p.current {
+  case 0: return p.person.FirstName
+  case 1: return p.person.MiddleName
+  case 2: return p.person.LastName
+  }
+  panic("We should not be here!")
+}
+
 func main() {
-  p := Person{"Alexander", "", "Bell"}
+  p := Person{"Alexander", "Graham", "Bell"}
   //for _, name := range p.Names() {
   //  fmt.Println(name)
   //}
-  for name := range p.NamesGenerator() {
-    fmt.Println(name)
+
+  //for name := range p.NamesGenerator() {
+  //  fmt.Println(name)
+  //}
+
+  for it := NewPersonNameIterator(&p); it.MoveNext(); {
+    fmt.Println(it.Value())
   }
 }
