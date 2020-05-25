306211fd8c0298a6328b1fe411ee85acbde91753
Joe Chen <joe.chen.sydney@gmail.com>
2020-05-25 12:20:55 +1000

    Go code for Iterator Pattern, added go chanel for generator

--- 03-Behavioral-Design-Patterns/15-Iterator/Go/Iterator-Iterator/Iterator-Iterator.go
+++ 03-Behavioral-Design-Patterns/15-Iterator/Go/Iterator-Iterator/Iterator-Iterator.go
@@ -10,9 +10,25 @@ func (p *Person) Names() []string {
   return []string{p.FirstName, p.MiddleName, p.LastName}
 }
 
+func (p *Person) NamesGenerator() <-chan string {
+  out := make(chan string)
+  go func ()  {
+    defer close(out)
+    out <- p.FirstName
+    if len(p.MiddleName) > 0 {
+      out <- p.MiddleName
+    } 
+    out <- p.LastName
+  }()
+  return out
+}
+
 func main() {
-  p := Person{"Alexander", "Graham", "Bell"}
-  for _, name := range p.Names() {
+  p := Person{"Alexander", "", "Bell"}
+  //for _, name := range p.Names() {
+  //  fmt.Println(name)
+  //}
+  for name := range p.NamesGenerator() {
     fmt.Println(name)
   }
 }
